8094bdcd82188900a8a8bebc0cf27d5723fb31f1
Wenhao Yang <wyang39@ucsc.edu>
2021-07-20 17:31:56 -0700

    Updated schema, data.sql

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -3,8 +3,17 @@ INSERT INTO dummy (created) VALUES (current_timestamp);
 
 -- Populate Your Tables Here --
 DELETE FROM users;
-INSERT INTO users(email, users) VALUES ('molly@books.com','{"email": "molly@books.com","password": "$2b$10$Y00XOZD/f5gBSpDusPUgU.iJufk6Nxx6gAoHRG8t2eHyGgoP2bK4y","role": "member","name": "Molly Member"}');
-INSERT INTO users(email, users) VALUES ('anna@books.com','{"email": "anna@books.com","password": "$2b$10$Y00XOZD/f5gBSpDusPUgU.G1ohpR3oQbbBHK4KzX7dU219Pv/lzze","role": "admin","name": "Anna Admin"}');
+INSERT INTO users(users) VALUES ('{"email": "molly@books.com","password": "$2b$10$Y00XOZD/f5gBSpDusPUgU.iJufk6Nxx6gAoHRG8t2eHyGgoP2bK4y","role": "member","name": "Molly Member"}');
+INSERT INTO users(users) VALUES ('{"email": "anna@books.com","password": "$2b$10$Y00XOZD/f5gBSpDusPUgU.G1ohpR3oQbbBHK4KzX7dU219Pv/lzze","role": "admin","name": "Anna Admin"}');
 
 DELETE FROM workspace;
-INSERT INTO workspace(id, workspace) VALUES ('Anna Admin','{"name":"CSE183 Summer 2021","channels": ["Assignment 1", "Assignment 2"], "directMessages": ["Bob Dylan", "Carole King"]}');
+INSERT INTO workspace(workspace) VALUES ('{"name":"CSE183 Summer 2021"}');
+
+DELETE FROM channel;
+INSERT INTO channel(workspace_id, channel) VALUES ('{"name":"Assignment 1"}');
+INSERT INTO channel(workspace_id, channel) VALUES ('{"name":"Assignment 2"}');
+INSERT INTO channel(workspace_id, channel) VALUES ('{"name":"Assignment 3"}');
+INSERT INTO channel(workspace_id, channel) VALUES ('{"name":"Assignment 4"}');
+INSERT INTO channel(workspace_id, channel) VALUES ('{"name":"General"}');
+
+
--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -3,26 +3,35 @@ DROP TABLE IF EXISTS dummy;
 CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
 
 -- Your database schema goes here --
+-- join table--
+--workspace_users--
+-- two cloumns, one workspace id, one for user id--
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), users jsonb);
 
 DROP TABLE IF EXISTS workspace;
 CREATE TABLE workspace(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), workspace jsonb);
--- join table--
---workspace_users--
--- two cloumns, one workspace id, one for user id--
+--json objects--
+--workspace_name--
 
 DROP TABLE IF EXISTS channel;
-CREATE TABLE channel(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), workspace id, channel jsonb);
---name--
+CREATE TABLE channel(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), workspace_id, channel jsonb);
+--json objects--
+--channel_name--
 --message--
 
 DROP TABLE IF EXISTS message;
-CREATE TABLE message(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), channel id, message jsonb);
+CREATE TABLE message(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), channel_id, message jsonb);
+--json objects--
+--from_user--
+--text--
 
 DROP TABLE IF EXISTS directmessage;
-CREATE TABLE directmessage(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), from-user, to-user, directmessage jsonb);
-
+CREATE TABLE directmessage(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), from_user, to_user, directmessage jsonb);
+--json objects--
+--text--
+--status--
 
 
 
